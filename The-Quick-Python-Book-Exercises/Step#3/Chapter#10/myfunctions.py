892a3091702467da2db944bb1f8fd287df0a1dd7
Dorrin Samadian <82513219+dorrinsam@users.noreply.github.com>
2022-08-22 16:25:06 +0430

    These functions are related to Lab10.

---
+++ The-Quick-Python-Book-Exercises/Step#3/Chapter#10/myfunctions.py
@@ -0,0 +1,25 @@
+def lower(l):
+    lowline = l.lower()
+    return lowline
+
+def delete_punctuation(l):
+    s = l.maketrans('.,?!:;','      ')
+    a = l.translate(s)
+    return a
+
+def words(l):
+    words = l.split()
+    c = '\n'.join(words)
+    return c
+
+def count(y):
+    x = {}
+    for b in y:
+        x[b] = x.get(b, 0) + 1
+    return x
+
+def most_used(x):
+    list_1 = list(x.items())
+    list_1.sort(key = lambda x : x[1])
+    print(list_1[0], list_1[-1])
+
